790358ac4af1a05d4942a6fa6a61d8cd95d816c0
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-09-28 12:19:10 -0400

    working form components

---
+++ sea-levels/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const app = fire.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+})
+
+
+export const auth = app.auth()
+export default app
