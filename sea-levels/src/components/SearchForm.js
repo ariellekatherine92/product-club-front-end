d2149ef2b53cbface5fe07ed20421441e4873cd7
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-10-05 22:35:55 -0400

    added style to landing page made address form

---
+++ sea-levels/src/components/SearchForm.js
@@ -0,0 +1,28 @@
+import React, { useState, useCallback } from 'react';
+
+const SearchForm = () => {
+    const [locationInput, setLocationInput] = useState('');
+
+    const onSubmit = useCallback(e => {
+        e.preventDefault();
+
+        console.log(locationInput);
+    }, [locationInput]);
+
+    return (
+        <form className="search-form" onSubmit={onSubmit}>
+            <input 
+                type="text" 
+                name="location"
+                placeholder="Enter an Address or Zip Code"
+                onChange={({ target }) => setLocationInput(target.value)}
+            />
+            <button 
+                type="submit">
+                Get Started
+            </button>
+        </form>
+    )
+};
+
+export default SearchForm;
