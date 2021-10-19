3b01caec3b59097733ad49af180b10bbc84a9036
mnebot <miguelnbt@icloud.com>
2021-10-19 19:56:23 -0400

    -created emegencies pages -fetch data from firestore -renders on emegencies

--- sea-levels/src/screens/Emergencies/Emergencies.js
+++ sea-levels/src/screens/Emergencies/Emergencies.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import app from "../../services/firebase";
 
-const Emergencies = () => {
+const Emergencies = ({ user }) => {
+  const [alerts, setAlerts] = useState([]);
+
+  useEffect(() => {
+    const fetchAlerts = async () => {
+      const db = app.firestore();
+      const doc = await db
+        .collection(`emergencies`)
+        .get()
+        .then((querySnapshot) => {
+          const data = querySnapshot.docs.map((doc) => doc.data());
+          setAlerts(data);
+        });
+    };
+    fetchAlerts();
+  }, []);
+
   return (
     <div>
-      
+      <h1>Ongoing Emegencies</h1>
+      <ul>
+        {alerts.map((alert) => (
+          <ol>
+            {alert.name} {alert.location} {alert.type} {alert.needs}{" "}
+            {alert.dateTime.toLocaleString()} {alert.active}
+          </ol>
+        ))}
+      </ul>
     </div>
   );
 };
