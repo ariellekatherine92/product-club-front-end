156a5b55423b5b90ddf22b067383db42250e039e
mnebot <miguelnbt@icloud.com>
2021-10-21 19:13:02 -0400

    fix nav link, created signout componented and moved signout logic from dashboard to signout screen.

--- sea-levels/src/components/Navbar.js
+++ sea-levels/src/components/Navbar.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
 
 export default function Navbar(props) {
   const unauthenticated = (
-    <>
+    <div className='unauth-links'>
       <Link to="/signup">
         <div className="signup-btn">Signup</div>
       </Link>
-
       <Link to="/login">Login</Link>
-    </>
+    </div>
   );
 
   const authenticated = (
-    <>
-      <Link className='link' to="/dashboard">Dashboard</Link>
+    <div className='auth-links'>
+      <Link to="/dashboard">Dashboard</Link>
 
-      <Link  to="/signout">Sign Out</Link>
+      <Link to="/signout">Sign Out</Link>
 
       {/* <li className="nav-user-name">{props.client?.name}</li> */}
-    </>
+    </div>
   );
 
   return (
@@ -40,7 +39,9 @@ export default function Navbar(props) {
         <li>
           <Link to="/emergencies">Emergencies</Link>
         </li>
-        <li>{props.user ? authenticated : unauthenticated}</li>
+        <li>
+          {props.user ? authenticated : unauthenticated}
+        </li>
       </ul>
     </nav>
   );
