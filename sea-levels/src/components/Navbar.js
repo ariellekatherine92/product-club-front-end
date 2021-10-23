0ccc1972d2a991640a85ded6b339c98b990c550d
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-10-23 00:18:37 -0400

    Fix Navbar Style

--- sea-levels/src/components/Navbar.js
+++ sea-levels/src/components/Navbar.js
@@ -1,48 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-
-export default function Navbar(props) {
-  const unauthenticated = (
-    <div className='unauth-links'>
-      <Link to="/signup">
-        <div className="signup-btn">Signup</div>
-      </Link>
-      <Link to="/login">Login</Link>
-    </div>
-  );
-
-  const authenticated = (
-    <div className='auth-links'>
-      <Link to="/dashboard">Dashboard</Link>
-
-      <Link to="/signout">Sign Out</Link>
-
-      {/* <li className="nav-user-name">{props.client?.name}</li> */}
-    </div>
-  );
-
-  return (
-    <nav className="page-nav">
-      <ul>
+const AuthLinks = ({ isAuthenticated }) => isAuthenticated ? (
+    <>
         <li>
-          <button
-            className="sos-btn"
-            onClick={() => props.setIsOpen(!props.isOpen)}
-          >
-            SOS
-          </button>
+            <Link to="/dashboard">Dashboard</Link>
         </li>
         <li>
-          <Link to="/">Home</Link>
+            <Link to="/signout">Sign Out</Link>
+        </li>
+        {/* <li className="nav-user-name">{props.client?.name}</li> */}
+    </>
+) : (
+    <>
+        <li>
+            <Link to="/login">Login</Link>
         </li>
         <li>
-          <Link to="/emergencies">Emergencies</Link>
-        </li>
-        <li>
-          {props.user ? authenticated : unauthenticated}
-        </li>
-      </ul>
-    </nav>
-  );
-}
+            <Link to="/signup">
+                <button className="signup-btn">Signup</button>
+            </Link>
+        </li> 
+    </>
+);
+
+const Navbar = ({ user, isOpen, setIsOpen }) => {
+    const toggleIsOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="page-nav">
+            <ul>
+                <li>
+                    <button
+                        className="sos-btn"
+                        onClick={toggleIsOpen}>
+                        SOS
+                    </button>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/emergencies">Emergencies</Link>
+                </li>
+                    
+                <AuthLinks isAuthenticated={!!user} />
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
