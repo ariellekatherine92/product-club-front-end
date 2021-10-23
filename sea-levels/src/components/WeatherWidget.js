edf795f39c05e000ee4ebc1fae70ec98a686a1b8
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-10-22 21:18:41 -0400

    Weather Card

--- sea-levels/src/components/WeatherWidget.js
+++ sea-levels/src/components/WeatherWidget.js
@@ -1,20 +1,60 @@
-import React from "react";
-import "weather-icons/css/weather-icons.css";
-import { useSelector } from "react-redux";
-import coolicon from '../images/coolicon.png'
-import './WeatherWidget.css'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import contact from '../images/ic-contact.png';
+import location from '../images/ic-location.png';
+import users from '../images/ic-users.png';
+import home from '../images/ic-home.png';
+import uv from '../images/ic-uv.png';
+import 'weather-icons/css/weather-icons.css';
+import './WeatherWidget.css';
 
-const WeatherWidget = ({weather}) => {
-  const location = useSelector((state) => state.location)
-  // console.log(location)
+const WeatherWidget = ({ username, weather, ...props  }) => {
+  const zip = useSelector(({ location }) => location);
+
   return (
-    <div className="container">
-      <h1 className="weather-name">Mary S.</h1>
-      <img src={coolicon} alt="cool icon" />
-      <p>{weather.location?.name}, {weather.location?.region} {location}</p>
-      <p>{weather.current?.temp_f}</p>
-      <p>Feels like {weather.current?.feelslike_f}</p>
-      <p>uv {weather.current?.uv}</p>
+    <div className="weather-widget">
+      <div className="user-details">
+        <h5><img src={contact} /> {username}</h5>
+
+        <span>
+          <div className="img-wrapper">
+            <img src={location} />
+          </div>
+          <span>{`${weather.location?.name}, ${weather.location?.region} ${zip}`}</span>
+        </span>
+
+        <span>
+          <div className="img-wrapper">
+            <img src={users} />
+          </div>
+          <span>Occupants: 5</span>
+        </span>
+        
+        <span>
+          <div className="img-wrapper">
+            <img src={home} />
+          </div>
+          <span>
+            Status: <span className="safe">safe</span>
+          </span>
+        </span>
+      </div>
+
+      <div className="weather-details">
+        <div className="conditions">
+          <img className="weather-icon" src={weather?.current?.condition?.icon} />
+          <span className="temp">{weather.current?.temp_f}</span>
+        </div>
+
+        <div>
+          <span>{`Feels like ${weather.current?.feelslike_f}°`}</span>
+        </div>
+
+        <div className="uv">
+          <img src={uv} />
+          <span>{`${weather.current?.uv * 100}%`}</span>
+        </div>
+      </div>
     </div>
   );
 };
