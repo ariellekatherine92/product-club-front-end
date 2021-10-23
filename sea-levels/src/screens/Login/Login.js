fe19b82c136b7ff70d2fad4358930f8c00c03692
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-10-23 01:08:30 -0400

    Login Page

--- sea-levels/src/screens/Login/Login.js
+++ sea-levels/src/screens/Login/Login.js
@@ -1,37 +1,39 @@
-import React, {useRef, useState} from 'react'
-import {Form, Button, Card, Alert} from 'react-bootstrap'
-import { useAuth } from '../../contexts/AuthContext'
-import { Link, useHistory } from 'react-router-dom'
-
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
+import { Link, useHistory } from 'react-router-dom';
+import './Login.css';
 
 export default function Login() {    
-    const history = useHistory()
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const { login } = useAuth()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
+    const history = useHistory();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { login } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     async function handleSubmit(e) {
-        e.preventDefault()
+        e.preventDefault();
 
         try {
-            setError('')
-            setLoading(true)
-            await login(emailRef.current.value, passwordRef.current.value)
-            history.push('/')
+            setError('');
+            setLoading(true);
+            await login(emailRef.current.value, passwordRef.current.value);
+            history.push('/');
         } catch {
-            setError('Failed to log in')
+            setError('Failed to log in');
         }
-        setLoading(false)
+        setLoading(false);
     }
 
     return (
-        <div>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Log In</h2>
-                    {error && <Alert variant= "danger"> {error} </Alert>}
+        <div className="login-page">
+            <div className="bg" />
+            <div className="form-content">
+                <div className="wrapper">
+                    <h5>Welcome back</h5>
+                    <h2>Login to continue</h2>
+
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
@@ -41,15 +43,24 @@ export default function Login() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">Log In</Button>
+                        <div className="form-options">
+                            <div>
+                                <input type="radio" id="remember-me" />
+                                <label for="remember-me">Remember me</label>
+                            </div>
+
+                            <div>
+                                <span>Forgot password?</span>
+                            </div>
+                        </div>
+                        <Button disabled={loading} className="w-100" type="submit">LOG IN</Button>
                     </Form>
-                    <div className="w-100 text-center mt-3">
-                        <Link to="/forgot-password">Forgot Password</Link>
+
+                    <div className="join">
+                        <span>Don't have an account? <Link to= "/signup">Join free today</Link></span>
                     </div>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2"></div>
-            Don't have an account? <Link to= "/signup">Sign up Free!</Link>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
