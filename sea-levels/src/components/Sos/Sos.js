990fa26ae6c8e0f531768c10fbd5f9d37ca875bf
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-11-03 23:01:26 -0400

    sos styles

--- sea-levels/src/components/Sos/Sos.js
+++ sea-levels/src/components/Sos/Sos.js
@@ -1,112 +1,123 @@
-import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom"
-import app from "../../services/firebase";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import app from '../../services/firebase';
 import './Sos.css'
 
-const Sos = (props) => {
-  const [alert, setAlert] = useState({})
-  const [form, setForm] = useState({
-    name: "",
-    location: "",
-    needs: [],
-    type: "",
-    dateTime: new Date().toLocaleString(),
-    email: '',
-    
-  });
-  
-  const history = useHistory();
-
-  const deleteAlert = async () => {
-    try {
-      const db = app.firestore()
-      await db.collection('emergencies').doc(props.user).update({})
-      await db.collection('emergencies').doc(props.user).delete()
-  } catch (error) {
-    console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    const getOneEmergence = async () => {
-      const db = app.firestore()
-      const fetchOneEmergence = await db.collection('emergencies').doc(props.user).get()
-      const oneEmergence = fetchOneEmergence.data();
-      setAlert(oneEmergence)
-    }
-    getOneEmergence()
-  },[]);
-
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
+const Sos = ({ isOpen, setIsOpen, user }) => {
+    const [alert, setAlert] = useState({})
+    const [form, setForm] = useState({
+        dateTime: new Date().toLocaleString(),
+        name: '',
+        location: '',
+        emergency: '',
+        needs: '',
+        email: '',
     });
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try{
-      const db = app.firestore()
-      await db.collection('emergencies').doc(props.user).set({...form})
+    const history = useHistory();
 
-    } catch(error) {
-      throw Error
-    }
-    // props.setToggle(!props.Toggle)
-    props.setIsOpen(!props.isOpen)
-    history.push('/emergencies')
-  };
+    // const deleteAlert = async () => {
+    //     try {
+    //         const db = app.firestore();
+    //         await db.collection('emergencies').doc(user).update({});
+    //         await db.collection('emergencies').doc(user).delete();
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+
+    useEffect(() => {
+        const getOneEmergence = async () => {
+            const db = app.firestore();
+            const fetchOneEmergence = await db.collection('emergencies').doc(user).get();
+            const oneEmergence = fetchOneEmergence.data();
+            setAlert(oneEmergence);
+        };
+        getOneEmergence();
+    }, [user]);
 
 
-  return (
-    <div className="popup-box">
-      <form onSubmit={handleSubmit} className='box'>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={form.name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Location"
-          name="location"
-          value={form.location}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Needs"
-          name="needs"
-          value={form.needs}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Type"
-          name="type"
-          value={form.type}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          checked={form.email}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      <button className='close-icon' onClick={() => props.setIsOpen(!props.isOpen)}>X</button>
-      {props.content}
-      {alert ? <div>{alert?.name} {alert?.location} {alert?.needs} {alert?.type} {alert?.email} <button onClick={deleteAlert}>X</button></div>:''}
-      
-      </form>
-    </div>
-  );
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const db = app.firestore();
+            await db.collection('emergencies').doc(user).set({ ...form });
+        } catch(error) {
+            throw Error;
+        }
+
+        setIsOpen(!isOpen);
+        history.push('/emergencies');
+    };
+
+
+    return (
+        <div className="popup-box">
+            <div className="click-to-close" onClick={setIsOpen.bind(this, false)} />
+
+            <form onSubmit={handleSubmit} className="sos-form">
+                <h2>SOS</h2>
+                <div className="input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={form.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Location"
+                        name="location"
+                        value={form.location}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Emergency"
+                        name="emergency"
+                        value={form.emergency}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Needs"
+                        name="needs"
+                        value={form.needs}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        checked={form.email}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <button type="submit">Submit</button>
+
+                {/*alert ? (
+                    <div>
+                        {alert?.name} 
+                        {alert?.location} 
+                        {alert?.needs} 
+                        {alert?.type} 
+                        {alert?.email} 
+                        <button onClick={deleteAlert}>X</button>
+                    </div>
+                ) : null*/}
+            </form>
+        </div>
+    );
 };
 
 export default Sos;
