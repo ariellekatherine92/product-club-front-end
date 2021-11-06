77c7d9a7acf7d511f921c024387bcdd3b0bf4e07
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-11-05 23:46:31 -0400

    emergencies styles

--- sea-levels/src/screens/Emergencies/Emergencies.js
+++ sea-levels/src/screens/Emergencies/Emergencies.js
@@ -1,41 +1,72 @@
-import { useState, useEffect } from "react";
-import ButtonMailto from "../../components/ButtonMailto";
-import app from "../../services/firebase";
-
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import app from '../../services/firebase';
+import './Emergencies.css';
 
 const Emergencies = (props) => {
-  const [alerts, setAlerts] = useState([]);
-  
+    const [alerts, setAlerts] = useState([]);
 
-  useEffect(() => {
-    const fetchAlerts = async () => {
-      const db = app.firestore();
-      await db
-        .collection(`emergencies`)
-        .get()
-        .then((querySnapshot) => {
-          const data = querySnapshot.docs.map((doc) => doc.data());
-          setAlerts(data);
-          
-        });
-    };
-    fetchAlerts();
-  }, [props.toggleFetch]);
+    useEffect(() => {
+        const fetchAlerts = async () => {
+            const db = app.firestore();
+            await db.collection('emergencies').get().then((querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => doc.data());
+                setAlerts(data);
+            });
+        };
 
-  return (
-    <div>
-      <h1>Ongoing Emergencies</h1>
-      <ul>
-        {alerts.map((alert) => (
-          <ol>
-            {alert.name} {alert.location} {alert.type} {alert.needs}
-            {alert.dateTime.toLocaleString()}  
-            <ButtonMailto label='E-Mail' mailto={'mailto:'+ alert.email}/>
-          </ol>
-        ))}
-      </ul>
-    </div>
-  );
+        fetchAlerts();
+    }, [props.toggleFetch]);
+
+    console.log(alerts);
+
+    return (
+        <div className="emergencies-container">
+            <div className="table-wrapper">
+                <div className="table-header">
+                    <div className="title">
+                        <h3>Ongoing Emergencies</h3>
+                    </div>
+                    <div className="controls" />
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Contact</th>
+                            <th>Location</th>
+                            <th>Emergency</th>
+                            <th>Needs</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {alerts.map(({ dateTime, emergency, location, name, needs }, idx) => (
+                            <tr key={`alert-${dateTime}-${idx}`}>
+                                <td className="contact">
+                                    <div>
+                                        <div className="avatar" />
+                                        {name}
+                                    </div>
+                                </td>
+                                <td><div>{location}</div></td>
+                                <td><div>{emergency}</div></td>
+                                <td><div>{needs}</div></td>
+                                <td className="date-time">
+                                    <div>
+                                        <span className="date">{moment(dateTime).format('ll')}</span>
+                                        <br/>
+                                        <span className="time">{moment(dateTime).format('LT')}</span>
+                                    </div>
+                                </td>
+                                <td><div>Active</div></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Emergencies;
