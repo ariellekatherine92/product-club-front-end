5933d903eba9030bf1beb32c65129e02c028dcbb
Eric Steger <ericlawrence.gd@gmail.com>
2021-11-10 01:28:43 -0500

    Contact Form Style

--- sea-levels/src/components/NewContactPopup/NewContactPopup.js
+++ sea-levels/src/components/NewContactPopup/NewContactPopup.js
@@ -1,21 +1,26 @@
-import { useState } from "react";
-import {v4 as uuidv4 } from 'uuid'
-import app from "../../services/firebase";
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import app from '../../services/firebase';
+import './NewContactPopup.css';
 
-const NewContactPopup = (props) => {
+const NewContactPopup = ({ setIsOpen, user }) => {
   const [contactForm, setContactForm] = useState({
-    name: "",
-    city: "",
-    state: "",
-    zipCode: "",
+    name: '',
+    city: '',
+    state: '',
+    zipCode: '',
     alert: '',
     contactID: uuidv4(),
-    userID: props.user,
+    userID: user,
   });
 
+  const close = () => {
+    setIsOpen(false);
+  };
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
+
     setContactForm({
       ...contactForm,
       [name]: value,
@@ -24,65 +29,73 @@ const NewContactPopup = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const db = app.firestore();
-      await db.collection("contacts").add({ ...contactForm });
+
+      await db.collection('contacts').add({ ...contactForm });
+
       setContactForm({
-      name: "",
-      city: "",
-      state: "",
-      zipCode: "",
-      icon: '',
-      temp:'',
-      alert: '',
-      userID:'',
-      })
+        name: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        icon: '',
+        temp:'',
+        alert: '',
+        userID:'',
+      });
     } catch (error) {
-      throw Error;
+      throw error;
     }
-    props.setIsOpen(!props.isOpen);
+
+    close();
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="box">
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={contactForm.name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="City"
-          name="city"
-          value={contactForm.city}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="State...example RI"
-          name="state"
-          value={contactForm.state}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Zip Code"
-          name="zipCode"
-          value={contactForm.zipCode}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-        <button
-          className="close-icon"
-          onClick={() => props.setIsOpen(!props.isOpen)}
-        >
-          X
-        </button>
-        {props.content}
-      </form>
+    <div className="contact-modal-container">
+      <div className="modal-background" onClick={close} />
+
+      <div className="contact-modal">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={contactForm.name}
+            onChange={handleChange}
+          />
+
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            value={contactForm.city}
+            onChange={handleChange}
+          />
+
+          <input
+            type="text"
+            placeholder="State"
+            name="state"
+            value={contactForm.state}
+            onChange={handleChange}
+          />
+
+          <input
+            type="text"
+            placeholder="Zip Code"
+            name="zipCode"
+            value={contactForm.zipCode}
+            onChange={handleChange}
+          />
+
+          <div className="button-wrapper">
+            <button onClick={close}>Cancel</button>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
